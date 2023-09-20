67a31e2fc41bb08940748bf12dab7e7993d7eaff
Jeon, Hyeon O <gusdh2510@gmail.com>
2023-09-20 10:31:01 +0900

    three

--- src/a201914059/Main.java
+++ src/a201914059/Main.java
@@ -2,7 +2,7 @@ package a201914059;
 
 public class Main {
 	public static void main(String[] args) {
-		Two.print();
 		System.out.println("one 201914059");
+		Three.print();
 	}
 }
---
+++ src/a201914059/Three.java
@@ -0,0 +1,5 @@
+package a201914059;
+
+public class Three {
+
+}
